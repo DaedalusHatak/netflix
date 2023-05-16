e71be10b08c133b995279e31762d8c97225aef6d
DaedalusHatak <pawelkic55@gmail.com>
2023-05-16 10:23:27 +0200

    added askedQuestions

---
+++ src/components/AskedQuestions.vue
@@ -0,0 +1,149 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+interface Question {
+  name: string
+  answer: string
+  isActive: boolean
+}
+const arrOfQuestions = ref<Question[]>([
+  {
+    name: 'What is Netflix?',
+    answer: `Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.
+    
+    You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!`,
+    isActive: false
+  },
+  {
+    name: 'How much does Netflix cost?',
+    answer: `Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from 29 zł to 60 zł a month. No extra costs, no contracts.`,
+    isActive: false
+  },
+  {
+    name: 'Where can I watch?',
+    answer: `Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.
+
+You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.
+Note that this is not official netflix page!`,
+    isActive: false
+  },
+  {
+    name: 'How do I cancel?',
+    answer: `Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.`,
+    isActive: false
+  },
+  {
+    name: 'Where can I watch?',
+    answer: `Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.
+
+You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.
+Note that this is not official netflix page!`,
+    isActive: false
+  },
+  {
+    name: 'Where can I watch?',
+    answer: `Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.
+
+You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.
+Note that this is not official netflix page!`,
+    isActive: false
+  }
+])
+</script>
+
+<template>
+  <div>
+    <h2>Frequently Asked Questions</h2>
+  <ul>
+   
+    <li v-for="question in arrOfQuestions">
+      <h3>
+        <button @click="question.isActive = !question.isActive">
+          {{question.name}}
+          <p class="add">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="default-ltr-cache-1tgeghn e164gv2o4"
+              data-name="Add"
+              alt=""
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M11 2V11H2V13H11V22H13V13H22V11H13V2H11Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </p>
+        </button>
+      </h3>
+      <div v-if="question.isActive" class="answer">
+        <p>
+          {{ question.answer }}
+        </p>
+      </div>
+    </li>
+  </ul>
+  </div>
+</template>
+
+<style scoped>
+li {
+  list-style: none;
+  margin: 0 0 0.5rem 0;
+}
+ul {
+    margin: 1.5rem 0 0;
+    padding: 0;
+
+
+}
+h2{
+    text-align: center;
+    font-size: 3rem;
+    font-weight: 900;
+   
+}
+h3 {
+  margin-bottom: 0.0625rem;
+}
+button {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: left;
+  cursor: pointer;
+  color: white;
+  background-color: rgba(45, 45, 45, 1);
+  border: none;
+  font-size: 1.5rem;
+  width: 100%;
+  max-height: 75rem;
+  padding: 1.5rem;
+  transition-duration: 250ms;
+  transition-property: background-color;
+}
+button:hover {
+  transition-timing-function: cubic-bezier(0.5, 0, 0.1, 1);
+  background-color: rgba(78, 78, 78, 1);
+}
+.add {
+  height: 36px;
+}
+.answer {
+    font-size: 1.5rem;
+    font-weight: 500;
+
+  white-space: pre-line;
+  letter-spacing: normal;
+  padding: 1.5rem;
+  background-color: rgb(45, 45, 45);
+}
+svg {
+  width: 36px;
+  height: 36px;
+}
+</style>
