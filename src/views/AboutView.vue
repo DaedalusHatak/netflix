f27bc4a549db99a6ee191c43d13159c8a8262f8a
DaedalusHatak <pawelkic55@gmail.com>
2023-05-19 00:18:34 +0200

    Login component design

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,76 +1,40 @@
+<script setup lang="ts">
+import BaseInput from '@/components/BaseInput.vue';
+</script>
+
 <template>
   <img class="hero-img" src="@/assets/hero.jpg" alt="" />
   <div class="shadow-hero-image"></div>
   <div class="hero-wrapper">
     <nav>
       <span class="logo-span"><img src="@/assets/daedalus.png" alt="" /> </span>
-    
     </nav>
     <div class="flex-hero">
 
-      <div class="header-hero">
+      <div class="form-hero">
         <h1>Sign In</h1>
-
-       
-        <GetStartedForm />
+        <form action="">
+         <div>
+          <BaseInput name="Email or phone number" type="email" :background="true"/>
+         </div>
+         <div>
+          <BaseInput name="Password" type="password"  :background="true"/>
+         </div>
+         <button class="get-started-button">
+                Sign In
+              </button>
+        </form>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-@media screen and (min-width: 600px) {
-  nav {
-    max-width: calc(100% - 4rem);
-  }
+h1{
+  text-align: left;
+  margin-bottom: 50px;
 }
-@media screen and (min-width: 1280px) {
-  nav {
-    max-width: calc(83.33333333333334% - 6rem);
-  }
-}
-@media screen and (min-width: 1920px) {
-  nav {
-    max-width: calc(66.66666666666666% - 6rem);
-  }
-}
-.info {
-  color: gold;
 
-  inset: 0;
-  margin: auto;
-  text-align: center;
-  h1 {
-    font-size: 2.5rem;
-    font-family: fantasy;
-    letter-spacing: 0.2ch;
-    text-decoration: underline;
-    font-weight: 600;
-  }
-  h3 {
-    font-size: 2rem;
-    padding-top: 1rem;
-    font-weight: 600;
-  }
-}
-@media (min-width: 720px) {
-  .info {
-    position: absolute;
-    inset: 0;
-    top: 70px;
-    h1 {
-      font-size: 2.8rem;
-      font-family: fantasy;
-      letter-spacing: 0.2ch;
-      text-decoration: underline;
-      font-weight: 600;
-    }
-    h3 {
-      font-size: 2rem;
-      font-weight: 600;
-    }
-  }
-}
 nav {
   display: flex;
   align-items: center;
@@ -92,17 +56,11 @@ nav {
   margin: auto;
 }
 .hero-img {
-  height: 100%;
-  width: 100%;
-
-  position: absolute;
-  object-fit: cover;
-  transform: scale(1.25) translateY(-10%);
-  overflow: hidden;
+  display: none;
 }
 .flex-hero {
-  display: flex;
-  flex-direction: column;
+
+
   padding: 9.875rem 0 4rem;
   min-height: 43.75rem;
 }
@@ -125,12 +83,24 @@ a {
   bottom: 0;
   left: 0;
 }
-.header-hero {
-  max-width: calc(100% - 4rem);
-  margin: auto;
+.form-hero {
+
+  
+padding: 4rem 1rem;
+  margin: 0 auto;
   color: white;
   text-align: center;
   font-size: 50px;
+ 
+}
+form{
+  display: flex;
+  gap: 20px;
+  flex-direction: column;
+  justify-content: center;
+  text-align: left;
+  position: relative;
+
 }
 .subtitle-hero {
   margin: 1rem 0 0;
@@ -143,25 +113,86 @@ a {
   fill: currentColor;
   display: block;
 }
-
-.sign-in-button {
-  z-index: 1000;
-  border: 0;
-  border-radius: 0.25rem;
-  cursor: pointer;
-  fill: currentColor;
-  position: relative;
-  transition-duration: 250ms;
-  transition-property: background-color, border-color;
-  transition-timing-function: cubic-bezier(0.9, 0, 0.51, 1);
-  vertical-align: text-top;
-  width: auto;
-  line-height: 1;
-  font-size: 0.875rem;
+.get-started-button {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  writing-mode: horizontal-tb !important;
+  width: 100%;
+  margin-top:1rem;
+  flex: 0 0 auto;
+  border: 0px;
+ 
+  font-size: 1.5rem;
   font-weight: 500;
-  min-height: 2rem;
-  padding: 0.25rem 0.95rem;
-  background: rgb(229, 9, 20);
+  min-height: 3.5rem;
+  padding: 0.75rem 1.5rem;
+  background: rgb(51, 26, 187,0.9);
+  cursor: pointer;
   color: rgb(255, 255, 255);
+  border-radius: 1rem;
+}
+
+
+@media screen and (min-width: 600px) {
+  nav {
+    max-width: calc(100% - 4rem);
+  }
+  .hero-img {
+  height: 100%;
+  width: 100%;
+display:block !important;
+  position: absolute;
+  object-fit: cover;
+  transform: scale(1.25) translateY(-10%);
+  overflow: hidden;
+}
+.flex-hero {
+
+max-width: 450px;
+margin: auto;
+ padding: 9.875rem 0 4rem;
+ min-height: 43.75rem;
+}
+  .form-hero {
+
+  min-width: 500px;
+  padding: 4rem 4rem;
+  max-width: calc(100% - 4rem);
+  margin: 0 auto;
+  color: white;
+  text-align: center;
+  font-size: 50px;
+  background-color: rgba($color: #000000, $alpha: 0.8);
+}
+}
+@media screen and (min-width: 1280px) {
+  nav {
+    max-width: calc(83.33333333333334% - 6rem);
+  }
+}
+@media screen and (min-width: 1920px) {
+  nav {
+    max-width: calc(66.66666666666666% - 6rem);
+  }
+}
+
+@media (min-width: 720px) {
+  .info {
+    position: absolute;
+    inset: 0;
+    top: 70px;
+    h1 {
+      font-size: 2.8rem;
+      font-family: fantasy;
+      letter-spacing: 0.2ch;
+      text-decoration: underline;
+      font-weight: 600;
+    }
+    h3 {
+      font-size: 2rem;
+      font-weight: 600;
+    }
+  }
 }
 </style>
