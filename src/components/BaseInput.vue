49cfb9f7ebbeebef57fc861bf0d048d08e26b103
DaedalusHatak <pawelkic55@gmail.com>
2023-05-19 00:18:11 +0200

    autocomplete based on type

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,22 +1,37 @@
 <script setup lang="ts">
+import { onMounted,watch} from 'vue'
 import { ref } from 'vue'
 const props = defineProps<{
   name: string
   type: string
+  background: boolean
+ 
 }>()
 const isActive = ref<boolean>(false)
 const inputData = ref<string>('')
+
+watch(inputData, (newValue) => {
+  inputData.value = newValue
+})
+onMounted(() => {
+ watch;
+})
+
 </script>
 
 <template>
   <label :class="isActive ? 'label-active' : 'label'" for="emailfield">{{ props.name }}</label>
   <input
+
+:class="props.background ? 'background' : ''"
     :type="props.type"
     v-model="inputData"
     @focusout="inputData === '' ? (isActive = false) : (isActive = true)"
+
+    @change="inputData === '' ? (isActive = false) : (isActive = true)"
     @focusin="isActive = true"
     :is-active="isActive"
-    autocomplete="email"
+    :autocomplete="props.type"
     id="emailfield"
     minlength="5"
     maxlength="50"
@@ -24,16 +39,17 @@ const inputData = ref<string>('')
 </template>
 
 <style scoped lang="scss">
-
+.background{
+background-color: #333 !important;
+}
 input {
+  display: block;
   color: white;
   padding: 1.5rem 1rem 0.5rem;
   border: none;
   border-radius: 1rem;
-  font-size: inherit;
+  
 
-  min-height: 16px;
-  min-width: 16px;
   width: 100%;
   background-clip: padding-box;
   appearance: none;
@@ -41,13 +57,18 @@ input {
 
 }
 
-input:-webkit-autofill{
-  transition: all 5s;
-  -webkit-text-fill-color: white;
-  -webkit-background-clip: text !important;
-}
+
+ 
+    // Background color
+    input:-webkit-autofill {
+      -webkit-text-fill-color: #fff;
+      -webkit-background-clip: text;
+    }
+
+
 
 .label {
+  width: max-content;
   position: absolute;
   z-index: 1;
   white-space: nowrap;
